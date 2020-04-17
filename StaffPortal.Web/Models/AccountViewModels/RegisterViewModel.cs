29142f90bfb36a85b3e127651be88c3bc8910d73
Han Chee <hanchee@codecultivation.com>
2020-04-17 12:26:21 +0100

    Remove constraint on phone number

--- StaffPortal.Web/Models/AccountViewModels/RegisterViewModel.cs
+++ StaffPortal.Web/Models/AccountViewModels/RegisterViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace StaffPortal.Web.Models.AccountViewModels
 {
@@ -24,8 +20,6 @@ namespace StaffPortal.Web.Models.AccountViewModels
         [Required(ErrorMessage = "Please fill in your phone number.")]
         [Display(Name = "Phone No*:")]
         [DataType(DataType.PhoneNumber)]
-        [RegularExpression(@"^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$",
-            ErrorMessage = "Not a valid Phone number. Matching formats: +447222555555 | +44 7222 555 555 | (0722) 5555555")]
         public string PhoneNumber { get; set; }
 
         [Required]
@@ -34,13 +28,11 @@ namespace StaffPortal.Web.Models.AccountViewModels
         [Required]
         public string Username { get; set; }
 
-
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
