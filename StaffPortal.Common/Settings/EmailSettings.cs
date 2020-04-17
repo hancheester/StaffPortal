a4e9f556e73add984c3e78ca7b91f513f930053f
Han Chee <hanchee@codecultivation.com>
2020-04-17 12:56:16 +0100

    Add field "FromEmail"

--- StaffPortal.Common/Settings/EmailSettings.cs
+++ StaffPortal.Common/Settings/EmailSettings.cs
@@ -6,5 +6,6 @@
         public string UserName { get; set; }
         public string Password { get; set; }
         public string DisplayEmail { get; set; }
+        public string FromEmail { get; set; }
     }
 }
